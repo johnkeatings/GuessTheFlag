98bb4d08860976b95833499757236dcdcaa9bacc
John Keatings <keatingsjohn1@gmail.com>
2021-12-13 14:30:33 -0500

    Initial Commit

---
+++ Shared/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  Shared
+//
+//  Created by John Keatings on 2021-12-08.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
